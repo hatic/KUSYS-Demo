67e04a47c59388ef58669886c6e5c69dcac723f7
Hatice Çağlar Karatay <hatice.karatay@t-hos.com.tr>
2022-07-21 12:21:08 +0300

    Fluent validation added

---
+++ BusinessLayer/ValidationRoles/StudentValidator.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+
+namespace BusinessLayer.ValidationRoles
+{
+    public class StudentValidator:AbstractValidator<Student>
+    {
+        public StudentValidator()
+        {
+            RuleFor(x => x.FirstName).NotEmpty().WithMessage("The First Name field is required.");
+            RuleFor(x => x.LastName).NotEmpty().WithMessage("The Last Name field is required.");
+            RuleFor(x => x.BirthDate).NotEmpty().WithMessage("The Birthdate field is required.");
+            RuleFor(x => x.BirthDate).LessThan(DateTime.Now.Date).GreaterThan(DateTime.Now.Date.AddYears(-100)).WithMessage("Birthdate is not valid");
+            RuleFor(x => x.CourseId).NotEmpty().WithMessage("The Course field is required.");
+
+        }
+    }
+}
--- KUSYS-Demo/Controllers/StudentController.cs
+++ KUSYS-Demo/Controllers/StudentController.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRoles;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
@@ -24,6 +26,41 @@ namespace KUSYS_Demo.Controllers
         [HttpGet]
         public IActionResult AddUpdateStudent(int studentId)
         {
+            ViewData["StudentSelectList"] = GetCourses(studentId);
+            if (studentId == 0)
+                return View(new Student());
+            return View(studentManager.GetById(studentId));
+        }
+        [HttpPost]
+        public IActionResult AddUpdateStudent(Student student)
+        {
+            StudentValidator validator = new StudentValidator();
+            ValidationResult result = validator.Validate(student);
+            if (result.IsValid)
+            {
+                if (student.StudentId == 0)
+                    studentManager.AddStudent(student);
+                else
+                    studentManager.UpdateStudent(student);
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+                ViewData["StudentSelectList"] = GetCourses(student.StudentId);
+                return View(student);
+            }
+            return RedirectToAction("Index", "Student");
+        }
+        public IActionResult ViewDetails(int studentId)
+        {
+            var student = studentManager.GetStudentDetail(studentId);
+            return View(student);
+        }
+        private List<SelectListItem> GetCourses(int studentId)
+        {
             List<SelectListItem> items = courseManager.GetAllCourses().ConvertAll(a =>
             {
                 return new SelectListItem()
@@ -33,29 +70,10 @@ namespace KUSYS_Demo.Controllers
                     Selected = false
                 };
             });
-            ViewData["StudentSelectList"] = items;
             if (studentId == 0)
-            {
                 items.Add(new SelectListItem { Value = String.Empty, Text = "Select a course", Selected = true });
-                return View(new Student());
-            }
-            return View(studentManager.GetById(studentId));
+            return items;
         }
-        [HttpPost]
-        public IActionResult AddUpdateStudent(Student student)
-        {
-            if (student.StudentId == 0)
-                studentManager.AddStudent(student);
-            else
-                studentManager.UpdateStudent(student);
-            return RedirectToAction("Index","Student");
-        }
-        public IActionResult ViewDetails(int studentId)
-        {
-            var student = studentManager.GetStudentDetail(studentId);
-            return View(student);
-        }
-
 
     }
 }
